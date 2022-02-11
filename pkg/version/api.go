6efbaecb9c5bdf42473d846b60e8b13bffd04b7d
elliotxx <951376975@qq.com>
2022-02-11 19:48:18 +0800

    feat: set default version

--- pkg/version/api.go
+++ pkg/version/api.go
@@ -1,5 +1,29 @@
 package version
 
+import (
+	"runtime"
+	"time"
+)
+
+func init() {
+	if versionInfo == nil {
+		versionInfo = &Info{
+			ReleaseVersion: "develop",
+			GitVersion:     "develop",
+			GitCommit:      "",
+			GitTreeState:   "",
+			BuildTime:      time.Now().Format("2006-01-02 15:04:05"),
+			Runtime: RuntimeInfo{
+				GoVersion: runtime.Version(),
+				GOOS:      runtime.GOOS,
+				GOARCH:    runtime.GOARCH,
+				NumCPU:    runtime.NumCPU(),
+				Compiler:  runtime.Compiler,
+			},
+		}
+	}
+}
+
 func ReleaseVersion() string {
 	return versionInfo.ReleaseVersion
 }
